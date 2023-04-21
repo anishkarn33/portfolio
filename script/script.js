51c14998060041a585bb99bb6d63876bd7608e70
anujkarn002 <anujkarn002@gmail.com>
2023-04-21 21:49:41 +0545

    fix: :bug: hero section and fix google map location

--- script/script.js
+++ script/script.js
@@ -1,19 +1,65 @@
 // add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
-console.log(header)
-window.onscroll = function() {
-    const top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
-}
+const header = document.querySelector(".navbar");
+console.log(header);
+window.onscroll = function () {
+  const top = window.scrollY;
+  if (top >= 100) {
+    header.classList.add("navbarDark");
+    header.classList.remove("darkOverlay");
+  } else {
+    header.classList.remove("navbarDark");
+    header.classList.add("darkOverlay");
+  }
+};
+
+window.onload = function () {
+  const top = window.scrollY;
+  if (top >= 100) {
+    header.classList.add("navbarDark");
+    header.classList.remove("darkOverlay");
+  } else {
+    header.classList.remove("navbarDark");
+    header.classList.add("darkOverlay");
+  }
+};
 // collapse navbar after click on small devices
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
+const navLinks = document.querySelectorAll(".nav-item");
+const menuToggle = document.getElementById("navbarSupportedContent");
 
 navLinks.forEach((l) => {
-    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
-})
+  l.addEventListener("click", () => {
+    new bootstrap.Collapse(menuToggle).toggle();
+  });
+});
+
+const speed = 200; //milliseconds
+// hello in three different language
+const keywords = ["Hi", "Hola", "नमस्ते", "Ciao"];
+const element = document.getElementById("greeting");
+
+const sleep = (time) => new Promise((r) => setTimeout(r, time));
+
+const typingEffect = async (keywords) => {
+  for (const keyword of keywords) {
+    element.innerHTML = "";
+    for (const key of keyword) {
+      element.innerHTML += key;
+      await sleep(speed);
+    }
+    if (keyword === keywords[keywords.length - 1]) {
+      element.innerHTML += ".";
+      await sleep(1000);
+    } else {
+      await sleep(700);
+      for (const key of keyword) {
+        element.innerHTML = element.innerHTML.slice(0, -1);
+        await sleep(speed);
+      }
+      await sleep(700);
+    }
+  }
+  await sleep(1500);
+  typingEffect(keywords);
+};
+
+typingEffect(keywords);
